36e3e639ebb1a37d9638c58a28e6d6031b29d087
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-28 14:37:21 +0000

    added if which checks if nginx is already installed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,18 +2,18 @@
 # install NGINX and restart the service
 if [ "$(dpkg-query -W -f="${Status}" nginx 2>/dev/null | grep -c "ok installed")" -eq 0 ]
 then
-  sudo apt-get update
-  sudo apt-get install nginx -y
+  sudo apt-get update;
+  sudo apt-get install nginx -y;
 fi
 # creating folders needed if dosen't already exist
-mkdir -p ~/data/web_static/releases/test/
-mkdir -p ~/data/web_static/shared
-mkdir -p ~/data/web_static/current
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/current
 # creating html file
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > ~/data/web_static/releases/test/index.html
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 # creating simbolic link
-ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 # giving owner to the ubuntu user and group with the same group
-chown -R ubuntu:ubuntu ~/data/
+chown -R ubuntu:ubuntu /data/
 sudo sed -i '/\tserver_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
