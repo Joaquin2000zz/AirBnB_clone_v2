c0fdc7384420d14eec6b173b13461482cfd1a742
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-15 15:03:21 +0000

    fixed output message

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """main path to the flask application"""
-    return 'Hello HBNB!!'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
